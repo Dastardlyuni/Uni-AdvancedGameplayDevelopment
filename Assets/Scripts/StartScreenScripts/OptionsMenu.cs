9708f2dbe0a774b2d5c2e1b02acf4f0c74525221
Dom <D.leigh6@edu.salford.ac.uk>
2025-02-17 14:15:31 +0000

    Audio Mixer + Slider, Some Visual Settings + Main Menu
    --

--- Assets/Scripts/StartScreenScripts/OptionsMenu.cs
+++ Assets/Scripts/StartScreenScripts/OptionsMenu.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class OptionsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider volumeSlider;
 }
